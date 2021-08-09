6b7a1cb14ab075a2e18e394b7b62c0c0f118adb2
Vinícius da Silva Ribeiro <80132113+viniciussribeiro@users.noreply.github.com>
2021-08-09 13:02:12 -0300

    Update calculo.js

--- www/components/calculo.js
+++ www/components/calculo.js
@@ -6,9 +6,31 @@ window.onload = function(){
   const valor2 = document.querySelector("#valor2");
 
   const somar = document.querySelector("#somar");
+  
+
+  const subtrair = document.querySelector("#subtrair");
+  
+  const multiplicar = document.querySelector("#multiplicar");
+  
+  const dividir = document.querySelector("#dividir");
 
   somar.addEventListener("click", function(){
     let resultado = parseFloat(valor1.value) + parseFloat(valor2.value);
+    document.querySelector("#result").value = resultado;
+  });
+  
+ subtrair.addEventListener("click", function(){
+    let resultado = parseFloat(valor1.value) - parseFloat(valor2.value);
+    document.querySelector("#result").value = resultado;
+  }); 
+  
+  multiplicar.addEventListener("click", function(){
+    let resultado = parseFloat(valor1.value) * parseFloat(valor2.value);
+    document.querySelector("#result").value = resultado;
+  });
+  
+  dividir.addEventListener("click", function(){
+    let resultado = parseFloat(valor1.value) / parseFloat(valor2.value);
     document.querySelector("#result").value = resultado;
   });
 }
